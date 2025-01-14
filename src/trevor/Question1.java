04ca0525784f413f2df64ab4802d2ddf4ba26881
mackt <tdmack1992@gmail.com>
2025-01-13 20:33:13 -0500

    Question 1 Completed

--- src/trevor/Question1.java
+++ src/trevor/Question1.java
@@ -1,4 +1,23 @@
 package trevor;
 
+import java.sql.SQLOutput;
+
 public class Question1 {
+
+
+    /**
+     * Write  a method which can identify given number is even or odd
+     * Output ex:
+     * identify(5); ->"Odd"
+     * identify(6); ->"Even"
+     */
+
+    public static void identify(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
 }
+
--- src/trevor/Test.java
+++ src/trevor/Test.java
@@ -1,4 +1,12 @@
 package trevor;
 
+import static trevor.Question1.identify;
+
 public class Test {
+
+    public static void main(String[] args) {
+
+        identify(8);
+
+    }
 }
