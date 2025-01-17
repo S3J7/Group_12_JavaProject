e2c53ab08978d57dce72ae729062ce6614bd26c3
mohamed sahoui <mohamedsahoui062@gmail.com>
2025-01-17 10:57:56 -0500

    2nd question solution

--- src/mohamed/Question2.java
+++ src/mohamed/Question2.java
@@ -3,7 +3,7 @@ package mohamed;
 public class Question2 {
     public static void main(String[] args) {
 
-        /**
+        /*
          Write a function which prints out the numbers
          from 1 to 30 but for numbers which are a multiple of 3,
          print "FIN" instead of the number and
