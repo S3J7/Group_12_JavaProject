cc45ff806d71ee360209d7d8a7d7cb4b3b3df9db
estelabunjaj <estelabunjaj@icloud.com>
2025-01-28 01:05:21 -0500

    Question 2 solved!

--- src/Estela/Answer2.java
+++ src/Estela/Answer2.java
@@ -12,24 +12,18 @@ public class Answer2 {
          print "FINRA" instead of the number.
          */
         //.
+                for (int i = 1; i <= 30; i++) {
 
-        for (int i = 1; i <= 30; i++) {
-
-            boolean divBy3 = i % 3 == 0;
-            boolean divBy5 = i % 5 == 0;
-
-            String result = "";
-
-            if (divBy3 && divBy5) {
-                result = "FINRA";
-            } else if (divBy3) {
-                result = "FIN";
-            } else if (divBy5) {
-                result = "RA";
-            } else {
-                result = " " + i;
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        System.out.println(i + " FINRA");
+                    } else if (i % 5 == 0) {
+                        System.out.println(i + " RA");
+                    } else if (i % 3 == 0) {
+                        System.out.println(i + " FIN");
+                    } else {
+                        System.out.println(i);
+                    }
+                }
             }
-            System.out.println(result + " ");
-        }
+        //Question2
     }
-}
