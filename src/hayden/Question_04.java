0a82f3fd3e9138f8158cd75e59e036330baf6151
siriW <sijhoong@gmail.com>
2025-02-10 10:27:13 -0500

    Question 4-5 are done

--- src/hayden/Question_04.java
+++ src/hayden/Question_04.java
@@ -7,6 +7,33 @@ public class Question_04 {
      Write a return method that can reverse negative number and return it as int
      */
 
+    public static int reverseNegativeNumber(int num) {
 
+        if (num < 0) {
+            int reversed = 0;
+            num = -num;
+
+            while (num != 0) {
+
+                int digit = num % 10;
+
+                reversed = reversed * 10 + digit;
+
+                num /= 10;
+            }
+
+            return -reversed;
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int input = -123;
+
+        int result = reverseNegativeNumber(input);
+
+        System.out.println("result = " + result);
+    }
 
 }
