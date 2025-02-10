e7e7b6ef1c113ace3101b1822d703c688c212b63
siriW <sijhoong@gmail.com>
2025-02-10 10:36:51 -0500

    Question 5 is done

--- src/siriwarin/Question5.java
+++ src/siriwarin/Question5.java
@@ -9,31 +9,38 @@ public class Question5 {
      */
 
     public static String frequencyOfChars(String str) {
+
         String result = "";
 
 
         while (str.length() > 0) {
+
             char currentChar = str.charAt(0);
+
             int count = 0;
 
-
             for (int i = 0; i < str.length(); i++) {
+
                 if (str.charAt(i) == currentChar) {
+
                     count++;
                 }
             }
 
-            result += currentChar + Integer.toString(count);
+            result += currentChar + "" + count;
 
-            str = str.replaceAll(String.valueOf(currentChar), "");
+            str = str.replaceAll("" + currentChar, "");
         }
 
         return result;
     }
 
     public static void main(String[] args) {
+
         String input = "AAABBCDD";
-        String output = frequencyOfChars(input);
-        System.out.println("output = " + output);
+
+        String result = frequencyOfChars(input);
+
+        System.out.println("result = " + result);
     }
 }
