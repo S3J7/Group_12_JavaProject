57161690bff0536817a71855dbe29fbd617d3543
Henife <hanipa.memet@gmail.com>
2025-02-13 09:35:41 -0500

    updated question 1

--- src/henife/EvenOrOdd.java
+++ src/henife/EvenOrOdd.java
@@ -1,6 +1,9 @@
 package henife;
 
 public class EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println(identify(11));
+    }
         public static String identify(int num){
             if(num%2==0){
                 return "even";
