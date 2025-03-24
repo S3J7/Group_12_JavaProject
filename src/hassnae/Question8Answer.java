16bd8ea01f5e362c5c506d2ba5a16f072a2e11f9
ha05051988 <be_happy_0505@hotmail.com>
2025-03-24 11:12:31 -0400

    Question 8 Answer updated.

--- src/hassnae/Question8Answer.java
+++ src/hassnae/Question8Answer.java
@@ -12,6 +12,7 @@ public class Question8Answer {
 
         public static void main(String[] args) {
             System.out.println(reverse("ABCD"));
+            System.out.println(reverse("HMDB"));
         }
     }
 
